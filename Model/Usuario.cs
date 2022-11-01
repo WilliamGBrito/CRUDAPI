478d2e882a17b4077bb5e176c06331f098966324
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-11-01 17:54:35 -0300

    Criação contexto

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -8,7 +8,7 @@ namespace CRUDAPI.Model
     public class Usuario
     {
         public int Id { get; set; }
-        public string? Nome { get; set; } 
+        public string Nome { get; set; } 
         public DateTime DataNascimento { get; set; }
     }
 }
