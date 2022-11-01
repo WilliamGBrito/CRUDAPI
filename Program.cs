00a7bb6d5fdf5d888201f8342e90d5103bf2f30c
WilliamGBrito <will-dragoesdareal@hotmail.com>
2022-11-01 17:56:53 -0300

    config services || add UseSqlServer

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using CRUDAPI.Data;
+using CRUDAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UsuarioContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    });
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
